8608b42b8e09c9b0aab3fba74cf812227ab0972a
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2021-10-09 11:18:45 +0530

    commit

--- daimond.c
+++ daimond.c
@@ -3,12 +3,24 @@ int main(){
     int row,i,space,j;
     printf("Enter the number of rows you want to print");
     scanf("%d",&row);
-    for(i=1;i<=row;i++){
-        for(space=0;space<row-i;space++){
+    for(i=0;i<=row;i++){
+        for(space=0;space<=row-i;space++)
+        {
             printf(" ");
 
         }
-        for(j=1;j!=2*i-1;j++){
+        for(j=1;j<=2*i-1;j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    for(i=row-1;i>0;i--){
+        for(space=0;space<=row-i;space++)
+        {
+            printf(" ");
+
+        }
+        for(j=1;j<=2*i-1;j++){
             printf("%d",j);
         }
         printf("\n");
