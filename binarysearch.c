8e4ef9970cb01945ef0e50ee871f4eaa9d03abf1
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2021-10-12 20:24:33 +0530

    commit

--- binarysearch.c
+++ binarysearch.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int arr[10],n,i,j,k=0,value,flag;
+    int arr[10],n,i,j,k=0,d;
     printf("Enter the size of the array\n");
     scanf("%d",&n);
     printf("\nEnter the elements of the array\n");
@@ -11,19 +11,23 @@ int main(){
       for(i=0;i<n;i++){
         printf("%d",arr[i]);
       }
-    printf("\nEnter the value for serch\n");
-    scanf("%d",&value);
-        for(i=0,j=n;i<=j;){
-            k=(i+j)/2;
-            if(arr[i]==value){
-                printf("\nThe element is found at %d",k);
-            }
-           if(arr[i]<value){
-            i=k+1;
-            }s
-            else {
-            j=k-1;
-            }
-        }
+    printf("\nEnter the value for search\n");
+   scanf("%d,&d");
+   printf("%d",d);
+   for(i=0,j=n;i<=j;){
+       k=(i+j)/2;
+       if(arr[i]=d){
+           printf("\nThe element is found at%d",k);
+           break;
+       }
+       else if(arr[i]<d){
+           i=k+1;
+       }
+       else
+       j=k-1;
+       
+
+       }
+   
         return 0;
     }
