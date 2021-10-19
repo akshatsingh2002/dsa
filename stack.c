aa38c44dceb855e5b5916434847247f8a6008a2b
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2021-10-19 18:16:57 +0530

    commit

--- stack.c
+++ stack.c
@@ -1,5 +1,34 @@
-#inlude<stiod.h>
+#include<stdio.h>
+int i,top,stack[5],maxtack=5;
+void push(){
+    if(top==maxtack){
+        printf("Over flow :(");
+    }
+    else
+    printf("Enter elements in stack\n");
+while(top<maxtack){
+    scanf("%d",&stack[top]);
+    top++;
+}
+top=0;
+printf("\nThe stack is :");
+while(top<maxtack){
+    printf("%d",stack[top]);
+}
+}
 int main(){
- int 
+ int n;
+ printf("1. for push\n 2. for pop\n 3. for peep\n 4. for exit");
+ scanf("%d",&n);
+ switch(n){
+     case 1:push();
+     break;
+      case 2:pop();
+     break;
+      case 3:peep();
+     break;
+      case 4:return 0;
+     break;
+ }
     return 0;
 }
