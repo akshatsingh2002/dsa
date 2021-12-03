a778b3742441307dc9d1c5e4f5fa0f778b7c5e1e
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2021-12-03 20:46:28 +0530

    commit

---
+++ factorial.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int fact(int num){
+
+    if(num==0)
+    return 1;
+    else if (num==1)
+    return 1;
+    else
+    while(num>1)
+    return(num*fact(num-1));
+
+
+}
+int main(){
+int n;
+printf("Enter the number to find its factoria : ");
+scanf("%d",&n);
+printf("%d",fact(n));
+return 0;
+}
