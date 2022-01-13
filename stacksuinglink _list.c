543643a7fd259ff373a3e5ece48994ba35576327
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-01-13 09:32:16 +0530

    commit

---
+++ stacksuinglink _list.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<stdlib.h>
+struct stack{
+    int info;
+    struct  stack *next;
+    
+};
+typedef struct stack node;
+node *top=NULL;
+void push(int);
+int pop();
+void display();
+void main(){
+    int ch,i,item;
+    while(1){
+        printf("\n1.Push\n2.Pop\n3.Display\n4.Exit");
+        printf("\nEnter your choise");
+        scanf("%d",&ch);
+        switch (ch)
+        {
+        case 1:printf("\nEnter item");
+        scanf("%d",&item);
+        push(item);
+            break;
+        case 2:printf("\npoped item is %d",pop());
+        break;
+        case 3:printf("\nStack display");
+        break;
+        default:exit(0);
+            break;
+        }
+    }
+
+}
+void push(int item){
+    node *newnode;
+    newnode=(node*)malloc(sizeof(node));
+    newnode->info=item;
+    newnode->next=top;
+    top=newnode;
+}
+int pop(){
+    int x;
+    if(top==NULL)
+    printf("\nStack is underflow");
+    else{
+    x=top->info;
+    free(top);
+    top=top->next;
+    return x;
+    }
+    return 0;
+}
+void display(){
+    node *i;
+    printf("\nStack Display\n");
+    for(i=top;i!=NULL;i=i->next){
+        printf("%d",i->info);
+    }
+}
